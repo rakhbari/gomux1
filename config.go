4ff9aab06cc1ac36d19069cabc3dfde348751ecf
Ramin Akhbari <ramin@akhbari.us>
2022-03-22 20:07:12 -0700

    Changed all indentations to spaces (from tabs)

--- config.go
+++ config.go
@@ -1,12 +1,12 @@
 package main
 
 type Config struct {
-	Server struct {
-		Host string `yaml:"host", envconfig:"SERVER_HOST"`
-		Port string `yaml:"port", envconfig:"SERVER_PORT"`
-	} `yaml:"server"`
-	// Database struct {
-	//     Username string `yaml:"user", envconfig:"DB_USERNAME"`
-	//     Password string `yaml:"pass", envconfig:"DB_PASSWORD"`
-	// } `yaml:"database"`
+    Server struct {
+        Host string `yaml:"host", envconfig:"SERVER_HOST"`
+        Port string `yaml:"port", envconfig:"SERVER_PORT"`
+    } `yaml:"server"`
+    // Database struct {
+    //     Username string `yaml:"user", envconfig:"DB_USERNAME"`
+    //     Password string `yaml:"pass", envconfig:"DB_PASSWORD"`
+    // } `yaml:"database"`
 }
