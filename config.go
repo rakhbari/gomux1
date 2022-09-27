4608bb681c3ea8f6373cff5c8c4b7cd313b93e64
Ramin Akhbari <ramin@akhbari.us>
2022-09-27 16:41:19 -0700

    Standardized env variable name prefix

--- config.go
+++ config.go
@@ -9,8 +9,9 @@ type Config struct {
         Host string `env:"SERVER_HOST, default=0.0.0.0"`
         HttpPort int `env:"SERVER_HTTP_PORT, default=8080"`
         HttpsPort int `env:"SERVER_HTTPS_PORT, default=8443"`
-        CaCertPath string `env:"SERVER_CA_CERT_PATH, require=true"`
-        CaKeyPath string `env:"SERVER_CA_KEY_PATH, require=true"`
+        tlsCertPath string `env:"SERVER_TLS_CERT_PATH, require=true"`
+        tlsKeyPath string `env:"SERVER_TLS_KEY_PATH, require=true"`
+        tlsCaPaths []string `env:"SERVER_TLS_CA_PATHS"`
         WriteTimeout int `env:"SERVER_WRITE_TIMEOUT, default=15"`
         ReadTimeout int `env:"SERVER_READ_TIMEOUT, default=15"`
         IdleTimeout int `env:"SERVER_IDLE_TIMEOUT, default=60"`
