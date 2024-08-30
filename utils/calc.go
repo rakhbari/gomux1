f5a7bb8ef0488009ef1d8c0690511c280164cf15
Ramin Akhbari <ramin@akhbari.us>
2024-08-30 08:02:45 -0700

    Fixed tabs to spaces

--- utils/calc.go
+++ utils/calc.go
@@ -1,24 +1,24 @@
 package utils
 
 import (
-	"fmt"
-	"strings"
+    "fmt"
+    "strings"
 )
 
 var romanValuesMap = map[rune]int{
-	'I': 1,
-	'V': 5,
-	'X': 10,
-	'L': 50,
-	'C': 100,
-	'D': 500,
-	'M': 1000,
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000,
 }
 
 var subtractersMap = map[rune]string{
-	'I': "VX",
-	'X': "LC",
-	'C': "DM",
+    'I': "VX",
+    'X': "LC",
+    'C': "DM",
 }
 
 /*
@@ -27,40 +27,40 @@ Do we have a lastChar -> No? add it to the total
 Yes?
 */
 func RomanToInt(roman string) (int, error) {
-	var subtractor rune
-	var subtractors string
-	numTotal := 0
+    var subtractor rune
+    var subtractors string
+    numTotal := 0
 
-	// Validation the length of the pass-in string
-	if len(roman) < 1 || len(roman) > 15 {
-		return -1, fmt.Errorf("ERROR: Given roman numeral string must be between 0 and 15")
-	}
+    // Validation the length of the pass-in string
+    if len(roman) < 1 || len(roman) > 15 {
+        return -1, fmt.Errorf("ERROR: Given roman numeral string must be between 0 and 15")
+    }
 
-	for pos, char := range roman {
-		// Validate the char is in romanValuesMap
-		if _, exists := romanValuesMap[char]; !exists {
-			return -1, fmt.Errorf("ERROR: Given roman numeral string must only include allowed roman numeral characters. Offender: %c", char)
-		}
+    for pos, char := range roman {
+        // Validate the char is in romanValuesMap
+        if _, exists := romanValuesMap[char]; !exists {
+            return -1, fmt.Errorf("ERROR: Given roman numeral string must only include allowed roman numeral characters. Offender: %c", char)
+        }
 
-		if subtractor != 0 {
-			numTotal += romanValuesMap[char] - romanValuesMap[subtractor]
-			subtractor = 0
-		} else {
-			subtractors = subtractersMap[char]
-			// char is not a subtractor
-			if subtractors == "" {
-				numTotal += romanValuesMap[char]
-			} else {
-				// Is there another rune after char and is char a subtractor for that rune?
-				if pos < len(roman)-1 && strings.ContainsRune(subtractors, rune(roman[pos+1])) {
-					subtractor = char
-				} else {
-					numTotal += romanValuesMap[char]
-					subtractor = 0
-				}
-			}
-		}
-	}
+        if subtractor != 0 {
+            numTotal += romanValuesMap[char] - romanValuesMap[subtractor]
+            subtractor = 0
+        } else {
+            subtractors = subtractersMap[char]
+            // char is not a subtractor
+            if subtractors == "" {
+                numTotal += romanValuesMap[char]
+            } else {
+                // Is there another rune after char and is char a subtractor for that rune?
+                if pos < len(roman)-1 && strings.ContainsRune(subtractors, rune(roman[pos+1])) {
+                    subtractor = char
+                } else {
+                    numTotal += romanValuesMap[char]
+                    subtractor = 0
+                }
+            }
+        }
+    }
 
-	return numTotal, nil
+    return numTotal, nil
 }
--- utils/calc_test.go
+++ utils/calc_test.go
@@ -3,57 +3,57 @@ package utils
 import "testing"
 
 func TestRomanToInt(t *testing.T) {
-	type args struct {
-		roman string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    int
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-		{
-			name:    "III",
-			args:    args{roman: "III"},
-			want:    3,
-			wantErr: false,
-		},
-		{
-			name:    "LVIII",
-			args:    args{roman: "LVIII"},
-			want:    58,
-			wantErr: false,
-		},
-		{
-			name:    "MCMXCIV",
-			args:    args{roman: "MCMXCIV"},
-			want:    1994,
-			wantErr: false,
-		},
-		{
-			name:    "More than 15 char numeral MCMXCIVMCMXCIVIII",
-			args:    args{roman: "MCMXCIVMCMXCIVIII"},
-			want:    -1,
-			wantErr: true,
-		},
-		{
-			name:    "Invalid roman numeral character",
-			args:    args{roman: "IIaI"},
-			want:    -1,
-			wantErr: true,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := RomanToInt(tt.args.roman)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("RomanToInt() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if got != tt.want {
-				t.Errorf("RomanToInt() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+    type args struct {
+        roman string
+    }
+    tests := []struct {
+        name    string
+        args    args
+        want    int
+        wantErr bool
+    }{
+        // TODO: Add test cases.
+        {
+            name:    "III",
+            args:    args{roman: "III"},
+            want:    3,
+            wantErr: false,
+        },
+        {
+            name:    "LVIII",
+            args:    args{roman: "LVIII"},
+            want:    58,
+            wantErr: false,
+        },
+        {
+            name:    "MCMXCIV",
+            args:    args{roman: "MCMXCIV"},
+            want:    1994,
+            wantErr: false,
+        },
+        {
+            name:    "More than 15 char numeral MCMXCIVMCMXCIVIII",
+            args:    args{roman: "MCMXCIVMCMXCIVIII"},
+            want:    -1,
+            wantErr: true,
+        },
+        {
+            name:    "Invalid roman numeral character",
+            args:    args{roman: "IIaI"},
+            want:    -1,
+            wantErr: true,
+        },
+    }
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            got, err := RomanToInt(tt.args.roman)
+            if (err != nil) != tt.wantErr {
+                t.Errorf("RomanToInt() error = %v, wantErr %v", err, tt.wantErr)
+                return
+            }
+            if got != tt.want {
+                t.Errorf("RomanToInt() = %v, want %v", got, tt.want)
+            }
+        })
+    }
 }
